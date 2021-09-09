ea42f7fb22e323857081b7c68120a06aa5e41904
filipoo02 <filipsadowski.os@o2.pl>
2021-09-09 22:52:40 +0200

    Zmieniono wyswietlanie bledow w zaleznosci od node_env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,29 +1,32 @@
-const sendErrorApi = (err, req, res) => {
-  if (process.env.NODE_ENV === "dev") {
+const sendErrorDev = (err, req, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    err,
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  if (req.originalUrl.startsWith("/api")) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
-      stack: err.stack,
     });
   } else {
-    res.status(err.statusCode).json({
-      status: err.status,
+    res.status(err.statusCode).render("errorPage", {
       message: err.message,
+      statusCode: err.statusCode,
     });
   }
 };
-const sendErrorPage = (err, req, res) => {
-  res.status(err.statusCode).render("errorPage", {
-    message: err.message,
-    statusCode: err.statusCode,
-  });
-};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || "500";
   err.status = "error";
-  if (req.originalUrl.startsWith("/api")) {
-    sendErrorApi(err, req, res);
+  if (process.env.NODE_ENV === "dev") {
+    sendErrorDev(err, req, res);
   } else {
-    sendErrorPage(err, req, res);
+    sendErrorProd(err, req, res);
   }
 };
