a55f474fdf7d4c5575b6f22bde2dcacc1b5b8b44
filipoo02 <filipsadowski.os@o2.pl>
2021-09-09 22:53:12 +0200

    Refaktoryzacja, walidacja
    Dodano walidacje formatu adresow. Zapytanie do binga wlozono do funkcji getCoordsFromAddr(addr).

--- controllers/calcDistanceController.js
+++ controllers/calcDistanceController.js
@@ -3,43 +3,53 @@ const AppError = require("../utils/AppError");
 const { catchAsync } = require("../utils/catchAsync");
 const key = process.env.KEY;
 
+const isAddrFormat = (addr) => {
+  const splited = addr.trim().split(",");
+  if (splited.slice(3)[0]) {
+    return false;
+  }
+  const [street, city, country] = splited;
+  if (street && city && country) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const getCoordsFromAddr = async (addr) => {
+  let coords;
+
+  await axios({
+    method: "GET",
+    url: `http://dev.virtualearth.net/REST/v1/Locations/${addr}?o=json&key=${key}`,
+  }).then((result) => {
+    if (result.data.resourceSets[0].resources[0]) {
+      coords = result.data.resourceSets[0].resources[0].point.coordinates;
+    } else {
+      throw new AppError(`Cannot find address: ${addr}`, 400);
+    }
+  });
+
+  return coords;
+};
+
 const getCoords = catchAsync(async (req, res, next) => {
-  const address1 = req.body.address1;
-  const address2 = req.body.address2;
-  await axios({
-    method: "GET",
-    url: `http://dev.virtualearth.net/REST/v1/Locations/${address1}?o=json&key=${key}`,
-  }).then((result) => {
-    if (result.data.resourceSets[0].resources[0]) {
-      // console.log(result.data.resourceSets[0].resources[0].address);
-      req.body.firstAddress =
-        result.data.resourceSets[0].resources[0].point.coordinates;
-    } else {
-      throw new AppError(`Cannot find address: ${address1}`, 400);
-    }
-    // result.data?.resourceSets[0]?.resources[0]?.geocodePoints[0]?.coordinates;
-  });
-  await axios({
-    method: "GET",
-    url: `http://dev.virtualearth.net/REST/v1/Locations/${address2}?o=json&key=${key}`,
-  }).then((result) => {
-    if (result.data.resourceSets[0].resources[0]) {
-      // console.log(result.data.resourceSets[0].resources[0].address);
-      req.body.secondAddress =
-        result.data.resourceSets[0].resources[0].point.coordinates;
-    } else {
-      throw new AppError(`Cannot find address: ${address2}`, 400);
-    }
-    // result.data?.resourceSets[0]?.resources[0]?.geocodePoints[0]?.coordinates;
-  });
+  const firstAddr = req.body.firstAddress;
+  const secondAddr = req.body.secondAddress;
+
+  if (!isAddrFormat(firstAddr) || !isAddrFormat(secondAddr)) {
+    return next(new AppError("Invalid address format!", 404));
+  }
+
+  req.body.firstAddrCords = await getCoordsFromAddr(firstAddr);
+  req.body.secondAddrCords = await getCoordsFromAddr(secondAddr);
 
   next();
 });
+
 const getDistance = catchAsync(async (req, res, next) => {
-  const [lat0, long0] = req.body.firstAddress;
-  // console.log(req.body.firstAddress);
-  // console.log(req.body.secondAddress);
-  const [lat1, long1] = req.body.secondAddress;
+  const [lat0, long0] = req.body.firstAddrCords;
+  const [lat1, long1] = req.body.secondAddrCords;
 
   const result = await axios({
     method: "POST",
@@ -58,11 +68,19 @@ const getDistance = catchAsync(async (req, res, next) => {
       travelMode: "walking",
     },
   });
+
+  let distance =
+    result.data.resourceSets[0].resources[0].results[1].travelDistance * 1;
+
+  distance = Math.round(distance * 100) / 100;
+
+  // update travel distance for user
+  //
+
   res.status(200).json({
     status: "success",
     results: {
-      distance:
-        result.data.resourceSets[0].resources[0].results[1].travelDistance * 1,
+      distance,
     },
   });
 });
