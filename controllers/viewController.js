585175d03d89faa75e8a4bcf2599fd05ec85b83c
filipoo02 <filipsadowski.os@o2.pl>
2021-09-11 20:02:57 +0200

    zmieniono zapytania o dane do renderowania stron

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,23 +2,19 @@ const server = require("../server");
 const { catchAsync } = require("../utils/catchAsync");
 
 const homePage = catchAsync(async (req, res, next) => {
-  const results = server.getActivityFile("week", { sum: "sum" });
-  const dataWeek = server.getActivityFile("week", {
-    sum: "sum",
-    byday: "byday",
-  });
+  const results = await server.getWeeklyActivity();
+
   res.status(200).render("calcPage", {
     results,
-    dataWeek,
   });
 });
 
 const statisticsPage = catchAsync(async (req, res, next) => {
-  const results = server.getActivityFile("month", { byday: "byday" });
-  if (results.status === "fail") throw results;
-  res.status(200).render("statistics", {
-    results,
-  });
+  // const results = server.getActivityFile("month", { byday: "byday" });
+  // if (results.status === "fail") throw results;
+  // res.status(200).render("statistics", {
+  //   results,
+  // });
 });
 
 module.exports = { homePage, statisticsPage };
